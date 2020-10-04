70ce2fe5611ba37e09695e2b5a8d3bb6d4bef69f
valerif90 <valerif90@gmail.com>
2020-10-04 22:33:42 +0300

    Updates

---
+++ precache-manifest.adf8d57049106882dc73f0618df85b78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63c04d4ea4227ac05a101adc2fe7366",
+    "url": "/Barbara/index.html"
+  },
+  {
+    "revision": "36f3ad362b76aee355f7",
+    "url": "/Barbara/static/css/main.c6cc8249.chunk.css"
+  },
+  {
+    "revision": "21bb0c355b9ef64de94c",
+    "url": "/Barbara/static/js/2.e5d3da5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Barbara/static/js/2.e5d3da5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f3ad362b76aee355f7",
+    "url": "/Barbara/static/js/main.d071b3c3.chunk.js"
+  },
+  {
+    "revision": "6ee875840332558557e4",
+    "url": "/Barbara/static/js/runtime-main.e293afb1.js"
+  },
+  {
+    "revision": "cd2b80659a9c4d8bb2962b63bfc2b6aa",
+    "url": "/Barbara/static/media/preloader.cd2b8065.gif"
+  }
+]);
